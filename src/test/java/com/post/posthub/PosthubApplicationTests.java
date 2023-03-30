308f70c67d6885966e955eb9eecc61f0b31ff6ad
Harshita Gupta <harshitagupta@Harshitas-MacBook-Air.local>
2023-03-30 18:56:02 +0530

    Initial commit

---
+++ src/test/java/com/post/posthub/PosthubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.post.posthub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PosthubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
